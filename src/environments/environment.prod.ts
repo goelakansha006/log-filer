5368d505da276b6948bcbe81b5ccc54cb7ac5851
Akansha <akansha goyal>
2019-11-05 01:33:19 +0530

    env.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://row-spark.herokuapp.com'
+  apiUrl: 'https://row-spark.herokuapp.com/api'
+  apiUrlLogin: 'https://row-spark.herokuapp.com'
 };
